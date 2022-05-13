08c42897b6398e09a8d01f5a0a96a0b1084edd70
root_who <joaopaulo_gonzaga@outlook.com>
2022-05-13 01:01:56 -0300

    Container e informações de clima por horario, em um espaço de 6 horas adicionado

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Dimensions, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function App() {
 
@@ -9,7 +10,7 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <ScrollView >
         <SafeAreaView style={styles.container}>
-        <View style={styles.sts}>
+        <View style={styles.status_container}>
           <Text style={styles.city}>São Paulo</Text>
           <Text style={styles.current_degree}>16º</Text>
           <Text style={styles.other_info}>Pouco Nublado</Text>
@@ -18,16 +19,76 @@ export default function App() {
             <Text style={styles.other_info}>Min: 12º</Text>
         </View>
         </View>
-        <View style={styles.other_days_container}>
-          
+
+
+        {/* CONTAINER PRINCIPAL PARA TEMPERATURA POR HORARIO + FRASE DA PREVISÃO */}
+        <View style={styles.info_by_hour_container}> 
+
+          {/* CONTAINER CONTENDO A FRASE DA PREVISÃO MAIS LINHA DIVISORIA*/}
+          <View style={styles.info_day_view}>
+            <Text style={styles.info_day}>Previsão de clima nublado até 07:00</Text>
+          </View>
+
+          {/* CONTAINER PRINCIPAL DE ALINHAMENTO PARA TODAS AS 6 TEMPERATURAS, HORARIOS E ICONES*/}
+          <View style={styles.hours_container}>
+
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>Agora</Text>
+              <Icon name="cloud" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>22</Text>
+              <Icon name="cloud-sun" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>23</Text>
+              <Icon name="cloud-sun-rain" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>00</Text>
+              <Icon name="cloud-rain" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>01</Text>
+              <Icon name="cloud-moon-rain" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* CONTAINER DE ALINHAMENTO PARA CADA TEMPERATURAS, HORARIOS E ICONES*/}
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>02</Text>
+              <Icon name="sun" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            {/* <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>03</Text>
+              <Icon name="moon" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View>
+            <View style={styles.other_day_view}>
+              <Text style={styles.other_day}>04</Text>
+              <Icon name="cloud-moon" size={25} color="#fff"></Icon>
+              <Text style={styles.other_day}>22º</Text>
+            </View> */}
+
+          </View>
         </View>
-           <Text style={styles.other_info}>Open up App.js to start working on your app!</Text>   
+          
         </SafeAreaView>
       </ScrollView>
     </SafeAreaView>
   );
 }
 const width = Dimensions.get('screen').width;
+const widthOtherDays= width/1.11;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -35,12 +96,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  sts:{
+  status_container:{
+    marginTop:50,
     backgroundColor: '#00000000',
     alignItems: 'center',
     justifyContent: 'space-evenly',
     width: width,
-    marginBottom:70
+    marginBottom:30,
+    alignItems:'center'
   },
   city:{
     color:'#fff',
@@ -49,27 +112,66 @@ const styles = StyleSheet.create({
   },
   current_degree:{
     color:'#fff',
-    fontSize:120,
+    fontSize:110,
     fontWeight:'100'
   },
   other_info_container:{
     flexDirection:'row',
     justifyContent:'space-around',
     width: width/2.5,
-    
+    alignItems:'center'    
   },
   other_info:{
     color:'#fff',
     fontSize:18,
-    fontWeight:'300'
+    fontWeight:'500'
   },
-  other_days_container:{
-    backgroundColor:'rgb(42, 44, 78)',
-    width:width/1.115,
-    height:400,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+  info_by_hour_container:{
+     backgroundColor:'rgb(42, 44, 78)',
+      width:widthOtherDays,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      paddingTop:20,
+      height:200,
+      alignItems:'center'      
+  },
+  info_day_view:{
+    borderBottomWidth:0.25,
+    borderBottomColor:'rgb(190, 190, 190)',
+    width: widthOtherDays / 1.125,
+    alignItems:'center'
+  },
+  info_day:{
+    fontSize:16,
+    fontWeight:'300',
+    color:'#FFF',
+    marginBottom:20,
+    borderBottomWidth:5,
+    borderBottomColor:'#fff'
+  },
+  hours_container:{
+    paddingTop:25,
+    justifyContent:'space-around',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    paddingLeft:10,
+    paddingRight:10
+  },
+  other_day_view:{
+    flexDirection:'column',
+    width: widthOtherDays/7,
+    justifyContent:'space-around',
+    alignItems:'center',
+    height:100,
+    marginBottom:20,
+    
+  },
+  other_day:{
+    fontSize:16,
+    fontWeight:'500',
+    color:'#FFF'
   }
+
 });
